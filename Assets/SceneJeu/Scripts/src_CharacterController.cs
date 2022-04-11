14baded7d13db407b5338a1f5486adb3f55cc774
VincentDesj <37562907+VincentDesj@users.noreply.github.com>
2022-04-10 20:35:23 -0400

    Advancement on apparition of Particles.

---
+++ Assets/SceneJeu/Scripts/src_CharacterController.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class src_CharacterController : MonoBehaviour
+{
+    private GameObject character;
+    private List<Vector3> listOfPositions;
+    private src_FireController src_FireController;
+
+    public int targetIndex = -1;
+    private Vector3 targetPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GameObject.FindGameObjectWithTag("character");
+        listOfPositions = new List<Vector3>();
+        src_FireController = new src_FireController();
+
+
+        for (int i = 0; i < src_FireController.listOfFires.Length; i++) {
+            listOfPositions.Add(src_FireController.listOfFires[i].transform.position);
+        }
+
+        targetIndex = UnityEngine.Random.Range(0, listOfPositions.Count);
+        targetPosition = listOfPositions[targetIndex];
+        src_FireController.setVisibilityOn(targetIndex);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowTarget();
+    }
+
+    void FollowTarget()
+    {
+        character.GetComponent<NavMeshAgent>().SetDestination(targetPosition);
+
+        if (Vector3.Distance(transform.position, targetPosition) < .5f) { ChangeTarget(); }
+    }
+
+    private void ChangeTarget()
+    {
+        src_FireController.setVisibilityOff(targetIndex);
+
+        int nextTargetIndex = targetIndex;
+        while (nextTargetIndex == targetIndex) {
+           nextTargetIndex = UnityEngine.Random.Range(0, listOfPositions.Count);
+            Debug.Log(nextTargetIndex);
+        }
+        targetIndex = nextTargetIndex;
+
+
+        targetPosition = listOfPositions[targetIndex];
+        src_FireController.setVisibilityOn(targetIndex);
+    }
+}
---
+++ Assets/SceneJeu/Scripts/src_FireController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class src_FireController : MonoBehaviour
+{
+    public static ParticleSystem[] listOfFires;
+
+    void Awake()
+    {
+
+        listOfFires = this.GetComponentsInChildren<ParticleSystem>(true);
+        Debug.Log(listOfFires.Length);
+    }
+
+    public void setVisibilityOn(int target)
+    {
+            listOfFires[target].Play();
+    }
+
+    public void setVisibilityOff(int target)
+    {
+        listOfFires[target].Stop();
+    }
+}
